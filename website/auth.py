7212c122cbd270ee9dd1071206de97d048d7a3e9
Hinnerk Kuennen <hinnerk.kuennen@gmail.com>
2022-10-21 20:32:42 +0200

    typo fixed

--- website/auth.py
+++ website/auth.py
@@ -48,7 +48,7 @@ def sign_up():
         elif len(first_name) < 2:
             flash("First name must be greater than 1 character.", category='error')
         elif password1 != password2:
-            flash("Passwords don\t match.", category='error')
+            flash("Passwords don't match.", category='error')
         elif len(password1) < 7:
             flash("Password must be at least 7 characters.", category='error')
         else:
